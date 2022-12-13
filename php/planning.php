b892f66e6242bb7a714151e34dfb36803c21776d
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-12-13 22:47:31 +0100

    add test planning and reservation

--- php/planning.php
+++ php/planning.php
@@ -1,72 +1,197 @@
 <?php
+session_start();
+$login = $_SESSION['login'];
+mysqli_set_charset($connect,"utf8");
 
-/* $info = cal_info(0);
-var_dump($info);
-echo $info['months'][2]; */
-$jour = [
-    1 => 'Lundi',
-    2 => 'Mardi',
-    3 => 'Mercredi',
-    4 => 'Jeudi',
-    5 => 'Vendredi',
-    6 => 'Samedi',
-    7 => 'Dimanche',
-];
+$queryPlanning = mysqli_query($connect, "SELECT utilisateurs.login, reservations.titre, reservations.debut, reservations.fin, reservations.type_activité, reservations.id  FROM `utilisateurs` INNER JOIN reservations ON id_utilisateur=utilisateurs.id");
+$fetchPlanning = mysqli_fetch_all($queryPlanning, MYSQLI_ASSOC);
 
-if (isset($_GET['years']) AND preg_match("#^[0-9]{4}$#",$_GET['years'])) {
-    $annee = $_GET['years'];
-} else {
-    $annee = date('Y');
-}
-$NbrDeJour=[];
-for($mois=1;$mois<=12;$mois++) {
-    $NbrDeJour[$mois]=date("t",mktime(1,1,1,$mois,2,$annee));
-    $PremierJourDuMois[$mois]=date("w",mktime(5,1,1,$mois,1,$annee));
-}
+
+// $today=time();
+// $nextJour = time() + (24 * 60 * 60);
+//                    // 24 hours; 60 mins; 60 secs
+// echo 'Auj:       '. date('l d-m-y') ."\n";
+// echo 'Demain: '. date('l d-m-y', $nextJour) ."\n";
+
+            $monday = date('l d-m-y', strtotime('monday this week'));
+            $tuesday = date('l d-m-y', strtotime('tuesday this week'));
+            $wednesday = date('l d-m-y', strtotime('wednesday this week'));
+            $thursday = date('l d-m-y', strtotime('thursday this week'));
+            $friday = date('l d-m-y', strtotime('friday this week'));
+            $week=array($monday,$tuesday,$wednesday,$thursday, $friday);
+
+            $eight = "8:00";
+            $nine = "9:00";
+            $ten = "10:00";
+            $eleven = "11:00";
+            $twelve = "12:00";
+            $thirteen = "13:00";
+            $fourteen = "14:00";
+            $fifteen = "15:00";
+            $sixteen = "16:00";
+            $seventeen = "17:00";
+            $eighteen = "18:00";
+            $nineteen = "19:00"; 
+        $horaire=array($eight,$nine,$ten,$eleven,$twelve,$thirteen,$fourteen,$fifteen,$sixteen,$seventeen,$eighteen,$nineteen);
+        
 ?>
-<table id="recap">
-    <tr>
-        <td style="background:#FF8888;width:15px;height:15px;"></td><td>Réservé</td>
-    </tr>
-    <tr>
-        <td style="background:#88FF88;width:15px;height:15px;"></td><td>Disponible</td>
-    </tr>
-</table>
-<?php
-//$_SESSION[$NomDeSessionAdmin]=1;
-if(isset($_SESSION['login'])){
-    if(
-    isset($_GET['jour']) AND preg_match("#^[0-9]{1,2}$#",$_GET['jour']) AND
-    isset($_GET['mois']) AND preg_match("#^[0-9]{1,2}$#",$_GET['mois']) AND
-    isset($_GET['choix']) AND preg_match("#^(0|1)$#",$_GET['choix'])) {
-        if($_GET['choix']==1){
-            if(mysqli_query($connect,"INSERT INTO calendrier SET date='".$annee."-".$_GET['mois']."-".$_GET['jour']."'")) {
-                echo "Journée mise en \"réservé\" avec succès !";
-            } else {
-                echo "Une erreur s'est produite:<br />".mysqli_error($connect);
-            }
-        } else {
-            if(mysqli_query($connect,"DELETE FROM calendrier WHERE date='".$annee."-".$_GET['mois']."-".$_GET['jour']."'")) {
-                echo "Journée mise en \"disponible\" avec succès !";
-            } else {
-                echo "Une erreur s'est produite:<br />".mysqli_error($connect);
-            }
-        }
-    }
-}
-?>
+ <main>
+     <article id="general">
+        <section id="legende">
+                <h3>Légende</h3>
+            <div id="carrés">
+                <span class="leg"><div id="soc"></div><p class="key">social</p></span>
+                <span class="leg"><div id="loi"></div><p class="key">loisirs</p></span>
+                <span class="leg"><div id="sco"></div><p class="key">scolaire</p></span>
+                <span class="leg"><div id="spo"></div><p class="key">sport</p></span>
+                <span class="leg"><div id="fes"></div><p class="key">festivités</p></span>
+            </div>
+        </section>
+        <section id="tableau">
+            <table>
+                <thead>
+                    <!-- il faut le mm nb de td que de colonne en haut pour que ça marche -->
+                    <th></th>
+                    <?php foreach ($week as $jour){
+                        echo "<th>".$jour."</th>";                         
+                    }                   
+                 ?>
+                    </tr>
+                </thead>
+                <tbody>  
+                    <?php                         
 
+                        for ($i=0; isset($horaire[$i])==true; $i++){
+                            echo "<tr>";
+                            echo "<td>".$horaire[$i]."</td>";
+                            
+                         for ($j=0; isset($week[$j])==true; $j++){
+                             echo "<td>";   
 
-<section class="content_planning">
-<?php
- //affiche le jour actuel
-  $année= date('y');
-  $mois= date('m');
-  $jour= date('d') ;
-  echo '<div class ="ajd" align="center">Aujourd\'hui, nous sommes le : '. $jour;
-  echo '';
-  echo '/'.$mois;
-  echo '/'.$année;
-?>
+                            foreach ($fetchPlanning as $value){
+                            $debut = $value['debut'];
+                            $time = strtotime($debut);
+                            $jourDebut = date("l d-m-y",$time);
+                            $heureDebut = date("H:00",$time);
 
+                        //  echo $heureDebut;
+                        //  echo $jourDebut;
+                            if(($jourDebut == $week[$j]) && ($heureDebut == $horaire[$i])) {
+                                echo '<div class = "'.$value['type_activité'].'"><a href=./reservation.php?val='.$value['id'].'><span id="font">'. $value['titre'] . '</span><span class="tooltiptext">'.$value['login'].'</span>'.'</a></div>';?>
+                                <style>
+                                    #font {
+                                        font-family: 'Courier New', Courier, monospace;
+                                    }
+                                </style>
+                                <?php
+                                if ($value['type_activité'] == "social"){?> 
+                                    <style>.social  {background-color: #FFD3B4;
+                                    }
+                                    .social .tooltiptext {
+                                                visibility: hidden;
+                                                width: 120px;
+                                                background-color: #5D534A;
+                                                color: #FDFAF6;
+                                                text-align: center;
+                                                padding: 5px 0;
+                                                border-radius: 3px;
+                                                position: absolute;
+                                                z-index: 1;
+                                                }
+                            
+                                    .social:hover .tooltiptext {
+                                        visibility: visible;
+                                            }
+                                    </style>
+                                    <?php
+                                }elseif ($value['type_activité'] == "scolaire"){ ?>
+                                    <style>.scolaire  {background-color: #98DDCA;
+                                    }
+                                    .scolaire .tooltiptext {
+                                                visibility: hidden;
+                                                width: 120px;
+                                                background-color: #5D534A;
+                                                color: #FDFAF6;
+                                                text-align: center;
+                                                padding: 5px 0;
+                                                border-radius: 3px;
+                                                position: absolute;
+                                                z-index: 1;
+                                                }
+                            
+                                    .scolaire:hover .tooltiptext {
+                                        visibility: visible;
+                                            }
+                                    </style>
+                                    <?php
+                                }elseif ($value['type_activité'] == "loisirs"){ ?>
+                                    <style>.loisirs  {background-color: #FFAAA7;
+                                                }
+                                        .loisirs .tooltiptext {
+                                            visibility: hidden;
+                                            width: 120px;
+                                            background-color: #5D534A;
+                                            color: #FDFAF6;
+                                            text-align: center;
+                                            padding: 5px 0;
+                                            border-radius: 3px;
+                                            position: absolute;
+                                            z-index: 1;
+                                            }
+                        
+                                        .loisirs:hover .tooltiptext {
+                                            visibility: visible;
+                                                }
+                                        </style>
+                                    <?php
+                                }elseif ($value['type_activité'] == "sport"){ ?>
+                                    <style>.sport  {background-color: #D5ECC2;
+                                    }
+                                    .sport .tooltiptext {
+                                                visibility: hidden;
+                                                width: 120px;
+                                                background-color: #5D534A;
+                                                color: #FDFAF6;
+                                                text-align: center;
+                                                padding: 5px 0;
+                                                border-radius: 3px;
+                                                position: absolute;
+                                                z-index: 1;
+                                                }
+                            
+                                    .sport:hover .tooltiptext {
+                                        visibility: visible;
+                                            }
+                                    </style>
+                                    <?php
+                                }elseif ($value['type_activité'] == "festivite"){ ?>
+                                    <style>.festivite  {background-color: #F6DFEB;
+                                        }
+                                    .festivite .tooltiptext {
+                                            visibility: hidden;
+                                            width: 100px;
+                                            background-color: #5D534A;
+                                            color: #FDFAF6;
+                                            text-align: center;
+                                            padding: 5px 0;
+                                            border-radius: 3px;
+                                            position: absolute;
+                                            z-index: 1;
+                                            }
+                            
+                                    .festivite:hover .tooltiptext {
+                                        visibility: visible;
+                                            }  
+                                        </style><?php ;
+                                        }
+                                    }
+                                }
+                            }
+                         }
 
+                      ?>
+                </tbody>
+            </table>
+        </section>
+     </article>
+    </main>
--- php/reservation.php
+++ php/reservation.php
@@ -13,7 +13,97 @@ if (isset($_POST['submit_btn'])) {
     
 
 }
+$login = $_SESSION['login'];  
+        
 
+if(!empty($_POST)){
+    extract($_POST);
+    $validation=true;
+  }
+
+if(isset($_POST['reserver'])){
+
+    $titre=$_POST['titre'];
+    $description=$_POST['description'];
+    $type=$_POST['type'];
+
+    //Pour transformer le mode d'input - format de date vers celui de sqli afin de permettre plus tard la comparaison entre les deux
+        $debut=$_POST['debutdate'];
+        $time = strtotime($debut);
+    $newformatDebut = date("Y-m-d H:i:s",$time);
+    // Pour empêcher de s'inscrire le samedi et le dimanche 
+    $jourDebut = date('D',$time);
+    //pour empêcher de s'inscrire plus d'un jour
+    $jDebut = date('d',$time);
+    // pour vérifier l'heure par la suite - ne pas dépasser les 1h
+    $heureDebut = date("H:i:s",$time);
+
+
+        $fin=$_POST['findate'];
+        $time1 = strtotime($fin);
+    $newformatFin = date("Y-m-d H:00",$time1);
+    $jourFin = date('D',$time1);
+    $jFin = date('d',$time1);
+    $heureFin = date("H:00",$time1);
+
+    $uneHeure = date("1:00:00");
+ 
+
+    // on va d'abord vérifier les erreurs possibles : champs vides, créneaux déjà réservés, plus d'une heure, plus d'un jour, jour de fin antérieur au jour de fin == validation false
+    // si pas d'erreur alors tu me prends l'id de la session ET tu me rentres sa réservation
+
+
+    if(empty($titre)){
+        $validation = false;
+        $titreVide = "Veuillez rentrer un titre.";
+        echo $titreVide;     
+    }
+    if(empty($description)){
+        $validation = false;
+        $descriptionVide = "Veuillez rentrer une description.";
+        echo $descriptionVide;     
+    }
+
+    // //pour récupérer les dates afin de vérifier s'il y a disponibilité
+    $dateVerif=mysqli_query($connect, "SELECT `debut`, `fin` FROM `reservations`");
+    $dateFetch=mysqli_fetch_all($dateVerif, MYSQLI_ASSOC);
+
+    foreach($dateFetch as $date){
+
+        if($date['debut']==$newformatDebut && $date['fin']==$newformatFin){
+               $validation = false;
+               $verifErr = "Le créneaux est indisponible, veuillez-vous référer au planning et choisir un autre créneaux.";
+               echo $verifErr;      
+        }
+    }
+        if($newformatFin < $newformatDebut){
+            $validation = false;
+            $timeErr = "La date de fin est antérieure à la date de début, on ne peut remonter dans le temps !";
+            echo $timeErr;
+        }elseif (($jourDebut == "Sat" || $jourDebut == "Sun") || ($jourFin == "Sat" || $jourFin == "Sun")){
+            $validation=false;
+            $weekendErr = "Vous ne pouvez réserver la salle le week-end.";
+            echo $weekendErr;
+        }elseif($jFin-$jDebut>=1){
+            $validation=false;
+            $jourErr = "Vous ne pouvez réserver la salle que pour une heure le même jour.";
+            echo $jourErr;
+        }
+
+        if(@$heureFin - @$heureDebut > @$uneHeure){
+            $validation = false;
+            $heureErr = "Vous ne pouvez réserver la salle plus d'une heure.";
+            echo $heureErr;
+        }
+
+    if ($validation){
+        $requestId = mysqli_query($connect, "SELECT `id` FROM `utilisateurs` WHERE `login`='".$login."'");
+        $recupId = mysqli_fetch_assoc($requestId);
+        foreach ($recupId as $id){
+            $queryInsert=mysqli_query($connect, "INSERT INTO `reservations`(`titre`, `description`, `debut`, `fin`, `id_utilisateur`, `type_activité`) VALUES ('$titre','$description','$debut','$fin', '$id' ,'$type')");
+        }  
+    }
+ }
 
 ?>
 
