73ae58b57a4bf32191ecf9ea205108de1804a724
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-12-13 12:09:16 +0100

    ajout form

--- php/planning.php
+++ php/planning.php
@@ -1,18 +1,19 @@
 <?php
-$jour = [
-    'Lundi',
-    'Mardi',
-    'Mercredi',
-    'Jeudi',
-    'Vendredi',
-    'Samedi',
-    'Dimanche',
-];
 
-foreach ($jour as $key) {
-    echo "<div>" . $key . "</div>";
-    for ($h=0 ; $h <= 24 ; $h++) {
-        echo $h ."<br>";
-    }
-}
+$info = cal_info(0);
+var_dump($info);
+echo $info['months'][2];
+
+?>
+
+<section class="content_planning">
+<?php
+ //affiche le jour actuel
+  $année= date('y');
+  $mois= date('m');
+  $jour= date('d') ;
+  echo '<div class ="ajd" align="center">Aujourd\'hui, nous sommes le : '. $jour;
+  echo '';
+  echo '/'.$mois;
+  echo '/'.$année;
 ?>
---
+++ php/reservation.php
@@ -0,0 +1,12 @@
+<?php
+
+
+?>
+
+
+<form action="" method="get">
+    <label for="titre">Titre de l'event</label>
+    <input type="text" name="titre" id="log" required>
+    <label for="debut">Début de l'event</label>
+    <input type="datetime-local" name="" id="" value="" min="2022-12-13" required>
+</form>
