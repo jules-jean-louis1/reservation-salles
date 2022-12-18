c79f10b1249d4e836ec1387ad2d7898ec8116138
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-12-18 23:29:29 +0100

    fonction mais affiche q'un event

--- php/test3.php
+++ php/test3.php
@@ -19,10 +19,12 @@ if($week > 52) {
     $year--;
     $week = 52;
 }
+
+
 ?>
 
-<a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week == 52 ? 1 : 1 + $week).'&year='.($week == 52 ? 1 + $year : $year); ?>">Next  Week</a> <!--Next week-->
-<a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week == 1 ? 52 : $week -1).'&year='.($week == 1 ? $year - 1 : $year); ?>">Pre Week</a> <!--Previous week-->
+<a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week == 1 ? 52 : $week -1).'&year='.($week == 1 ? $year - 1 : $year); ?>"><< Semaine précédente</a> <!--Previous week-->
+<a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week == 52 ? 1 : 1 + $week).'&year='.($week == 52 ? 1 + $year : $year); ?>">Semaine suivante >></a> <!--Next week-->
 
 <table border="1px">
     <tr>
@@ -34,57 +36,56 @@ if($week > 52) {
             $hour = 8;
             $day = 1;
             $jours = 0;
+            for ($b = 0; isset($row[$b]); $b++) {
+                $event = $row[$b][1];
+                $ev = date('d M Y H:i', strtotime($event));
+                var_dump($ev);
                 while ($day <= 7) {
-                    for ($i=0; isset($jour[$i]) ; $i++) {
-                        $d = strtotime($year ."W". $week . $day); 
-                        echo "<td>".$jour[$i]. "<br>". date('d M Y', $d) ."</td>";
+                    for ($i = 0; isset($jour[$i]); $i++) {
+                        $d = strtotime($year . "W" . $week . $day);
+                        echo "<td>" . $jour[$i] . "<br>" . date('d M Y', $d) . "</td>";
                         $r[] = date('d M Y', $d);
                         $day++;
                     }
                 }
-                $t = strtotime($year ."W". $week . $day); 
+                $t = strtotime($year . "W" . $week . $day);
                 $r[] = date('d M Y', $t);
-                $five_days = array_slice($r,0,5,true);
-                
-                while($hour <= 19) { 
+                $five_days = array_slice($r, 0, 5, true);
+
+                while ($hour <= 19) {
                     echo "<tr>";
-                    for ($j=0; $j <= 0 ; $j++) {
-                            $time1 = $hour . ":00";
-                            $time2 = $hour . ":00". ":00";
-                            echo "<td>".$time1."</td>";
+                    for ($j = 0; $j <= 0; $j++) {
+                        $time1 = $hour . ":00";
+                        $time2 = $hour . ":00" . ":00";
+                        echo "<td>" . $time1 . "</td>";
+
+                    }
+
+
+                    for ($k = 0; isset($five_days[$k]); $k++) {
+                        if ($ev == $five_days[$k]. ' '.$time1) {
+                            echo "<td>" . $row[$b][0]. "</td>";
+                        } else {
+                            echo "<td>" . $five_days[$k] . " " . $time1 . "</td>";
                             
                         }
-                        for ($b=0; isset($row[$b]) ; $b++) {
-                            $event = $row[$b][1];
-                            $ev = date('d M Y H:i', strtotime($event));
-                            for ($k=0; isset($five_days[$k]) ; $k++) {
-                                
-                                if ($ev === $five_days[$k].' '.$time1) {
-                                    echo "<td>".$row[$b][0]."</td>";
-                                    
-                                } else {
-                                    echo "<td>".$five_days[$k]." ".$time1."</td>";
-                                    
-                                    
-                                    }
-                                }
-                                    
-                            }
-                                /* echo "<td>".$five_days[$k]." ".$time1."</td>"; */
-                                /* for ($j=4; $j <= 5 ; $j++) {
-                                    $time1 = $hour . ":00";
-                                    
-                                }  */
-                                /* echo "<td>".$five_days[$k]." ".$time1."</td>"; */
-                                /* echo "<td>".$time1."</td>"; */
-                            for ($j=5; $j <= 6 ; $j++) { 
-                                echo "<td>"."Indisponible"."</td>";
-                            }
+                    }
+
+
+                    /* for ($j=4; $j <= 5 ; $j++) {
+                    $time1 = $hour . ":00";
+                    
+                    }  */
+                    /* echo "<td>".$five_days[$k]." ".$time1."</td>"; */
+                    /* echo "<td>".$time1."</td>"; */
+                    for ($j = 5; $j <= 6; $j++) {
+                        echo "<td>" . "Indisponible" . "</td>";
+                    }
                     echo "</tr>";
                     $hour++;
-                    
-                } 
-                    
+
+                }
+            } 
                 
             
             
